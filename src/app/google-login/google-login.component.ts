f4853eabd1562bf379eb8ba8cd1ee11722aea736
hadiuzzaman909 <hadiuzzaman909@outlook.com>
2025-05-24 18:48:43 +0600

    added accesstoken

--- src/app/google-login/google-login.component.ts
+++ src/app/google-login/google-login.component.ts
@@ -1,5 +1,6 @@
 import { GoogleLoginProvider, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -11,8 +12,9 @@ import { Component } from '@angular/core';
 export class GoogleLoginComponent {
   user: SocialUser | null = null;
   loggedIn = false;
+  private accessToken = '';
 
-  constructor(private authService: SocialAuthService) {}
+  constructor(private authService: SocialAuthService,private httpClient: HttpClient) {}
 
   ngOnInit() {
     this.authService.authState.subscribe(user => {
@@ -24,10 +26,33 @@ export class GoogleLoginComponent {
 
   signInWithGoogle() {
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
-    
+
   }
 
   signOut() {
     this.authService.signOut();
   }
+
+
+
+
+
+  getAccessToken(): void {
+    this.authService.getAccessToken(GoogleLoginProvider.PROVIDER_ID).then(accessToken => this.accessToken = accessToken);
+  }
+
+  getGoogleCalendarData(): void {
+    if (!this.accessToken) return;
+
+    this.httpClient
+      .get('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
+        headers: { Authorization: `Bearer ${this.accessToken}` },
+      })
+      .subscribe((events) => {
+        alert('Look at your console');
+        console.log('events', events);
+      });
+  }
 }
+
+
